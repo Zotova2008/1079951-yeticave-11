4edf154ddd3f739ba5fcf6039e47d723c929f2de
Natali <n.z.work@yandex.ru>
2019-11-12 20:24:48 +0300

    Читаем и показываем записи из MySQL

--- templates/main.php
+++ templates/main.php
@@ -3,9 +3,9 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
 
-        <?php foreach ($categories as $value) : ?>
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $value; ?></a>
+        <?php foreach ($category as $value) : ?>
+            <li class="promo__item promo__item--<?= $value['symbol_cat']; ?>">
+                <a class="promo__link" href="pages/all-lots.html"><?= $value['category_name']; ?></a>
             </li>
         <?php endforeach; ?>
 
@@ -20,17 +20,17 @@
         <?php foreach ($ads as $item) : ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $item['img']; ?>" width="350" height="260" alt="">
+                    <img src="<?= $item['lot_img']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $item['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['title']; ?></a></h3>
+                    <span class="lot__category"><?= $item['category_name']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['lot_title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= format_amount($item['price']); ?></span>
+                            <span class="lot__cost"><?= format_amount($item['lot_price']); ?></span>
                         </div>
-                        <?php $time_array = get_time($item['date']); ?>
+                        <?php $time_array = get_time($item['date_final']); ?>
                         <div class="lot__timer timer <?php if ($time_array[0] < $limit_time) : ?>timer--finishing<?php endif; ?>">
                             <?= ($time_array[0] . ' : ' . $time_array[1]); ?>
                         </div>
